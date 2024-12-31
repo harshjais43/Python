9cf274d84504a6fb583b7715efaeafc69511e5bc
Harsh Jaiswal <2020pgiadharsh05@poornima.org>
2024-12-31 10:03:41 +0530

    Practice Files for day 1

---
+++ DAY1/typecasting.py
@@ -0,0 +1,30 @@
+# x = int(1)   
+# y = int(2.8) 
+# z = int("3") 
+
+# x = float(1)    
+# y = float(2.8)  
+# z = float("3") 
+# w = float("4.2")
+
+# x = frozenset(("apple", "banana", "cherry"))
+# print(x)
+
+# x = str("s1") 
+# y = str(2)   
+# z = str(3.0)  
+
+# # x = str(5)
+# # print(x)
+# # print(type(x)) 
+# # x = float(5)
+# # print(x)
+# # print(type(x))
+
+# x = str(3)
+# y = int(3)
+# z = float(3)
+# print(x)
+# print(y)
+# print(z)
+# print("hello world")
