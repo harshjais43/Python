9b66a0a763cc9e08bc70a33eaace7a31fdc9cc80
harshjais43 <2020pgiadharsh05@poornima.org>
2025-01-02 14:50:29 +0530

    files for day 3

---
+++ Day 3/dict.py
@@ -0,0 +1,82 @@
+# # thisdict = {
+# #   "brand": "Ford",
+# #   "model": "Mustang",
+# #   "year": 1964
+# # }
+# # print(thisdict)
+
+# # thisdict = {
+# #   "brand": "Ford",
+# #   "model": "Mustang",
+# #   "year": 1964,
+# #   "year": 2020
+# # }
+# # print(len(thisdict))
+
+# # type of dict
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# print(type(thisdict))
+
+# # access the elements 
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# x = thisdict["model"]
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+
+# x = thisdict.keys()
+# print(x)
+
+# # channge the value 
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# thisdict["year"] = 2018
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# thisdict.update({"year": 2020})
+
+# # remove
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# thisdict.pop("model")
+# print(thisdict)
+
+# nestes dict
+
+# myfamily = {
+#   "child1" : {
+#     "name" : "Emil",
+#     "year" : 2004
+#   },
+#   "child2" : {
+#     "name" : "Tobias",
+#     "year" : 2007
+#   },
+#   "child3" : {
+#     "name" : "Linus",
+#     "year" : 2011
+#   }
+# }
+
+# print(myfamily)
---
+++ Day 3/set.py
@@ -0,0 +1,37 @@
+# c = {"apple", "banana", "cherry", "apple"}
+# print(c)
+
+# v = {"apple", "banana", "cherry"}
+# print(len(v))
+
+# s = {"apple", "banana", "cherry"}
+# print(type(s))
+
+# set = {"apple", "banana", "cherry"}
+
+# for x in thisset:
+#   print(x)
+
+# set1 = {"apple", "banana", "cherry"}
+# print("banana" in set1)
+
+# thisset = {"apple", "banana", "cherry"}
+# thisset.remove("banana")
+# print(thisset)
+
+# thisset = {"apple", "banana", "cherry"}
+# thisset.discard("banana")
+# print(thisset)
+
+# thisset = {"apple", "banana", "cherry"}
+# thisset.clear()
+# print(thisset)
+
+# thisset = {"apple", "banana", "cherry"}
+# thisset.add("orange")
+# print(thisset)
+
+# thisset = {"apple", "banana", "cherry"}
+# tropical = {"pineapple", "mango", "papaya"}
+# thisset.update(tropical)
+# print(thisset)
